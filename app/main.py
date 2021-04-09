179780ee27c07159c7534184983e240e941a3a2a
Michalina Andrzejewska <andrmichgithub@mismail.xyz>
2021-04-09 22:51:45 +0200

    labels creation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import database
+import random
 from database import init_db
 from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware, db
@@ -18,6 +19,31 @@ class UsernameBody(BaseModel):
     username: str
 
 
+def get_items(filename):
+    labels = set()
+    with open(f"labels/{filename}","r" ) as file:
+        for line in file:
+            labels.add(line[:-1])
+    return list(labels)
+
+
+def create_user(room_id, username, filename):
+    unique_label = random.choice(get_items(filename))
+    while not check_label_unique(room_id, unique_label):
+        continue
+    user = User(username=username, room_id=room_id, label=unique_label)
+    db.session.add(user)
+    db.session.commit()
+
+
+def check_label_unique(room_id, label):
+    users = User.query.filter_by(room_id=room_id).all()
+    labels = []
+    for user in users:
+        labels.append(user.label)
+    return label not in labels
+
+
 class JoinRoomResponse(BaseModel):
     username: str
     room_id: str
@@ -26,11 +52,10 @@ class JoinRoomResponse(BaseModel):
 @app.post("/create", response_model=JoinRoomResponse)
 def create_room(username):
     room_id = uuid.uuid4().hex
-    user = User(username=username, room_id=room_id, label="label")
+    create_user(room_id, username, filename="animals.txt")
     room = Room(id=room_id)
     db.session.add(room)
-    db.session.add(user)
-    db.session.commit()
+
     return {"username": f"{username}", "room_id": f"{room_id}"}
 
 
