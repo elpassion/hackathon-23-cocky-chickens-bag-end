491b2194c744616775658eb9fabcc00631a415fe
Michalina Andrzejewska <andrmichgithub@mismail.xyz>
2021-04-09 18:50:29 +0200

    mocking data

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,19 +5,27 @@ app = FastAPI()
 
 @app.post("/create")
 def create_room(username):
-    pass
+    return {"username": f"{username}"}
 
 
 @app.get("/join")
 def read_root(username):
-    pass
+    return {"room_id": "room_id", "username": f"{username}"}
 
 
 @app.get("/status/{room_id}")
 def read_item(room_id):
-    pass
+    return {
+        "room_id": f"{room_id}",
+        "status": "on_air",
+        "players": [
+            {"username": "max", "label": "alpaca"},
+            {"username": "ann", "label": "dog"},
+            {"username": "adam", "label": "bat"},
+        ],
+    }
 
 
 @app.post("/start/{room_id")
 def start_room(room_id):
-    pass
+    return {"room_id": f"{room_id}"}
